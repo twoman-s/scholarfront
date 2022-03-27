bac631e0024c0744094b4e4477db1c2a88089623
AlexAllu <alexsasi545@gmail.com>
2022-03-27 17:56:44 +0530

    homebanner added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./../styles/Home.module.scss";
 
 export default function Home() {
   return (
@@ -12,7 +15,13 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div className={styles.homeBanner}>
+        <Image src="/homebanner.jpg" layout="fill" objectFit="cover" />
+        <div className={styles.bannerDetails}>
+          <h2>Get your scholarship today</h2>
+          <p>We help you to get your dream course at your dream college</p>
+        </div>
+      </div>
       <Link passHref href="/colleges">
         <h2>Colleges</h2>
       </Link>
